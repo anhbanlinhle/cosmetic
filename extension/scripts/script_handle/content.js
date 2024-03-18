61e02d36ef3d8d6d297fe429789fc28d7a3eb207
vmtuan12 <minhtuan3154@gmail.com>
2024-03-18 19:11:24 +0700

    feat[mtuan]: assign tag as important/unimportant

--- extension/scripts/script_handle/content.js
+++ extension/scripts/script_handle/content.js
@@ -1,19 +1,62 @@
-
-
-const excludedTags = new Set(["HEADER", "FOOTER", "IMG", "SVG", "INPUT", "SCRIPT", "LINK", "STYLE", "IFRAME"]);
-const excludedTagsMutation = new Set(["HEADER", "FOOTER", "IMG", "SVG", "INPUT", "SCRIPT", "LINK", "STYLE", "IFRAME", "BODY"]);
-let count = 0;
+const excludedTags = new Set(["HEADER", "FOOTER", "IMG", "SVG", "INPUT", "SCRIPT", "LINK", "STYLE", "IFRAME", "BUTTON", "desc"]);
+const excludedTagsMutation = new Set(["HEADER", "FOOTER", "IMG", "SVG", "INPUT", "SCRIPT", "LINK", "STYLE", "IFRAME", "BODY", "BUTTON", "desc"]);
+let countElImportant = 0;
+let countElUnimportant = 0;
 const config = { subtree: true, childList: true };
+
+const isTextElement = (element) => {
+  return Array.from(element.childNodes).every(child => child.nodeType === Node.TEXT_NODE);
+}
+
+const hasTextSibling = (element) => {
+  const nextSibling = element.nextSibling;
+  const previousSibling = element.previousSibling;
+  const nextElementSibling = element.nextElementSibling;
+  const previousElementSibling = element.previousElementSibling;
+
+  const nextSiblingTextNodeIsValid = (nextSibling !== null && nextSibling.nodeType == Node.TEXT_NODE && nextSibling.data.trim().length > 0);
+  const previousSiblingTextNodeIsValid = (previousSibling !== null && previousSibling.nodeType == Node.TEXT_NODE && previousSibling.data.trim().length > 0);
+  const nextElementSiblingIsValid = (nextElementSibling !== null && isTextElement(nextElementSibling));
+  const previousElementSiblingIsValid = (previousElementSibling !== null && isTextElement(previousElementSibling));
+
+  return (nextSiblingTextNodeIsValid || previousSiblingTextNodeIsValid || nextElementSiblingIsValid || previousElementSiblingIsValid);
+}
+
+const directTextElementIsImportant = (element) => {
+  // if (!isTextElement(element)) {
+  //   return false;
+  // }
+
+  let merged = "";
+
+  for (const child of element.childNodes) {
+    merged += child.data.trim();
+  }
+
+  // check pagination tag
+  if (element.tagName == "A" && !isNaN(Number(merged))) {
+    return false;
+  }
+
+  return (merged.length >= 15 && merged.includes(" ")) || hasTextSibling(element);
+}
 
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (!excludedTagsMutation.has(mutation.target.tagName) && mutation.addedNodes.length > 0) {
       for (const child of mutation.addedNodes) {  
-        traverse(child);
+        // traverse(child);
+        console.log(child);
       }
     }
   }
 };
+
+const hasDirectText = (element) => {
+  return Array.prototype.some.call(element.childNodes, function(child) {
+    return child.nodeType === Node.TEXT_NODE && /\S/.test(child.nodeValue);
+  })
+}
 
 const traverse = (element) => {
   if (excludedTags.has(element.tagName)) {
@@ -21,7 +64,13 @@ const traverse = (element) => {
   }
 
   if (hasDirectText(element)) {
-    console.log(element);
+    if (isTextElement(element) && !directTextElementIsImportant(element)) {
+      // console.log(element, " --- unimportant");
+      // countElUnimportant++;
+    } else {
+      countElImportant++;
+      console.log(element, element.tagName);
+    }
     console.log("----------------------------------");
   }
   
@@ -30,23 +79,18 @@ const traverse = (element) => {
   }
 }
 
-const hasDirectText = (element) => {
-  return Array.prototype.some.call(element.childNodes, function(child) {
-    return child.nodeType === Node.TEXT_NODE && /\S/.test(child.nodeValue);
-  })
-}
 
 window.addEventListener("load", (event) => {
   const observer = new MutationObserver(callback);
 
   const body = document.querySelector("body");
-
   observer.observe(body, config);
-  // console.log(body);
-  // setTimeout(() => {
-  //   traverse(body)
-  // }, 10000);
-  // console.log(count);
+  traverse(body);
+
+  setTimeout(() => {
+    console.log("important: ", countElImportant);
+    console.log("unimportant: ", countElUnimportant);
+  }, 10000);
 });
 
 // const RED = "#ff4a4d";
