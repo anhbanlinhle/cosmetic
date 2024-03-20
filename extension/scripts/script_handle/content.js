3d87558a1ec302df81032d9ddadfa9b0f45a6806
anhbanlinhle <vietlinhleviet@gmail.com>
2024-03-21 01:28:54 +0700

    feat[lynn]: encapsulate passive search function

--- extension/scripts/script_handle/content.js
+++ extension/scripts/script_handle/content.js
@@ -149,7 +149,7 @@ const traverse = async (element) => {
       // countElUnimportant++;
     } else {
       countElImportant++;
-      console.log(element, element.tagName);
+      // console.log(element, element.tagName);
       // console.log(element.textContent.trim());
       let color = await checkWordImportance(element.textContent.trim());
       element.style.background = COLOR[color]
@@ -175,26 +175,3 @@ window.addEventListener("load", (event) => {
 //     console.log("unimportant: ", countElUnimportant);
 //   }, 10000);
 });
-
-//SECTION - keyboard event
-
-let selectedText = null
-let selectedElement = null
-
-document.addEventListener('mouseup', (event) => {
-  selectedText = window.getSelection().toString()
-  selectedElement = window.getSelection().baseNode.parentElement
-  // selectedElement.classList.add("detected-product")
-})
-
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message.message === "trigger_search") {
-    // console.log(selectedText)
-    if (selectedText.length > 0) {
-      response = await queryElastic(selectedText)
-      console.log(response)
-    }
-    selectedText = ""
-    selectedElement = null
-  }
-})
